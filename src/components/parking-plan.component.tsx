bfd16903539d56124283b51d9b7441a8c085329f
Кирилл <91612813+jiuper@users.noreply.github.com>
2024-09-25 20:32:06 +0300

    fix

--- src/components/parking-plan.component.tsx
+++ src/components/parking-plan.component.tsx
@@ -152,7 +152,7 @@ export default function ParkingPlan({count}:ParkingPlan) {
   return (
     <>
       {isDesktop ? (
-        <Section className="px-[8.05rem] flex justify-center items-center">
+        <Section id={"parking"} className="px-[8.05rem] flex justify-center items-center">
           <div className="flex justify-center items-end gap-x-[7.25rem]">
             {selectedFloor === 4 ? (
               <Image
@@ -270,18 +270,18 @@ export default function ParkingPlan({count}:ParkingPlan) {
       ) : null}
 
       {!isDesktop ? (
-        <Section id="parking-plan" className="py-14 px-5">
+        <Section id={"parking"} className="py-14 px-5">
           <div className="pt-5 px-5 bg-grey-1 rounded-3xl">
             <div className={clsx(openDropDown ? "bg-white border-b-none rounded-t-3xl p-[25px]" : "rounded-full",'relative flex flex-col border border-grey-2 py-2 w-full mb-[25px]')}>
               <div className={clsx(cx("select-value","items-center justify-center flex", openDropDown  && "pb-[8px] border-b-grey-3 border-b-[1px]" ))} onClick={() => setOpenDropDown(!openDropDown)}>Этаж {selectedFloor + 1} - {listFloor[Number(selectedFloor) - 1]}</div>
               <div className={clsx(openDropDown ? "border-t-0 rounded-b-3xl px-[25px] gap-[12px] z-10" : "px-2","py-2 absolute bg-white left-[-1px] border border-grey-2 w-[100.5%]  top-[100%] items-start justify-center flex-col", openDropDown ? "flex" : "hidden")}>
               {FloorRecordEntries.map(([floor]) => (
-                    <div key={floor} onClick={() => {
+                    <Link href={`/${Number(floor)}#parking`} key={floor} onClick={() => {
                       handelHref(Number(floor))
                       setOpenDropDown(!openDropDown)
                     }} className={cx("select-value")}>
                       Этаж {Number(floor) + 1} - {listFloor[Number(floor) - 1]}
-                  </div>
+                  </Link>
               ))}
             </div>
             </div>
